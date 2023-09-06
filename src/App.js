2b67922506b0c8960717f1bc7f55130c87e34c6c
unknown <dinabeysinghe@gmail.com>
2023-09-06 19:34:21 +0530

    add constructor method

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 
+
+
 export default class App extends Component {
+  constructor() {
+    super();
+    console.log('constructor');
+  }
   render() {
+    console.log('work')
     return (
       <div>App</div>
     )
