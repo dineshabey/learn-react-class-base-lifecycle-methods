a05dafddb9f8332caf61cdc8f2635ad298a40e61
unknown <dinabeysinghe@gmail.com>
2023-09-07 17:39:46 +0530

    class base lifecycale methods

--- src/App.js
+++ src/App.js
@@ -3,14 +3,58 @@ import React, { Component } from 'react'
 
 
 export default class App extends Component {
+
+  state = {
+    count: 0
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+  componentDidMount() {
+    //before mount event (before rendaring)
+    //Data featching stage 
+    console.log('componentDidMount');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+
+  }
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate');
+
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+
+  }
+
   constructor() {
     super();
     console.log('constructor');
   }
+
+  addNumber = () => {
+    console.log('state change');
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+
   render() {
-    console.log('work')
+    console.log('render')
     return (
-      <div>App</div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div>
+          <p style={{ color: 'red', fontSize: '30px' }}>{this.state.count}</p>
+          <button onClick={this.addNumber} style={{ fontSize: '20px' }}>Add Number</button>
+        </div>
+      </div>
     )
   }
 }
